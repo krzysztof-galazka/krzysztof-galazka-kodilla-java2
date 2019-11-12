1f8dab65c6ee9d2dfdf57ce8efe9bf9adace23fc
krzysztof-galazka <kgalazka@vp.pl>
2019-11-12 12:35:35 +0100

    Add Calculator test and SimpleUser class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+//        //User test
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//        String result = simpleUser.getUsername();
+//
+//        if (result.equals("theForumUser")){
+//            System.out.println("test ok");
+//        } else {
+//            System.out.println("Error!");
+//        }
+
+        //Calculator test
+        Calculator calculator = new Calculator();
+
+        int addResult = Calculator.add(10,5);
+
+        if (addResult == 15){
+            System.out.println("Add test ok");
+        }else {
+            System.out.println("Error in adding!");
+        }
+
+        int substractResult = Calculator.substract(5,5);
+
+        if (substractResult == 5){
+            System.out.println("Substract test ok");
+        }else {
+            System.out.println("Error in substract!");
+        }
+
     }
 }
